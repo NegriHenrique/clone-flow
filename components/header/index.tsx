96d0a3cfc472313b341ce86e5a9e4b2f9e923a3c
Henrique Negri <henriquenegri84@gmail.com>
2021-03-02 11:27:35 -0300

    terminando header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import styles from '../../styles/Header.module.css';
 import { ImMenu3, ImMenu4 } from 'react-icons/im'
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header: React.FC = () => {
 	const [menuMobileAberto, setMenuMobileAberto] = useState(false)
@@ -16,6 +17,45 @@ const Header: React.FC = () => {
 							<ImMenu3 />
 						}
 					</button>
+					<div className={styles.menudesktop}>
+
+						<ul>
+							<li className={styles.selecionado}>
+								<Link href="/">
+									<a>Home</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/">
+									<a>Loja</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/">
+									<a>Episódios</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/">
+									<a>Concursos</a>
+								</Link>
+							</li>
+							<li>
+								<Link href="/">
+									<a>Membros</a>
+								</Link>
+							</li>
+							
+						</ul>
+
+						<div className={styles.menudesktopactions}>
+							<button className="btn btn-primary btn-outline btn-cabecalho">
+								flowcoins
+							</button>
+
+							<FaUserCircle color="FFF" size="20" title="fazer login"/>
+						</div>
+					</div>
 				</nav>
 			</header>
 
